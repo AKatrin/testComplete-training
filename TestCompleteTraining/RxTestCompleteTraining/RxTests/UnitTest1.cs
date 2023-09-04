681986a4ab629d7aa430ce240675eafc1633f69f
angelica lopez <angelica.lopez@jalasoft.com>
2023-09-04 15:42:30 -0400

    Init commit

---
+++ TestCompleteTraining/RxTestCompleteTraining/RxTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace RxTestCompleteTraining.RxTests
+{
+    using AutomatedQA.script;
+    using AutomatedQA.TestComplete;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
